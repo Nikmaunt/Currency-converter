3549d86e9180f33681a15abe631e4afbe4f3dd95
Nick <nikmaunt@gmail.com>
2023-10-29 21:16:14 +0400

    fix: bug fixing & refactoring

--- src/api/apiConstants.ts
+++ src/api/apiConstants.ts
@@ -1,7 +1,3 @@
-export const INITIAL_CURRENCIES_API = "http://localhost:8013/initial-currencies";
-export const CONVERT_API = "http://localhost:8013/convert";
-export const SORT_CURRENCIES_API = "http://localhost:8013/sort-currencies";
-
-// export const INITIAL_CURRENCIES_API = "https://curr-convert.onrender.com/initial-currencies";
-// export const CONVERT_API = "https://curr-convert.onrender.com/convert";
-// export const SORT_CURRENCIES_API = "https://curr-convert.onrender.com/sort-currencies";
+export const INITIAL_CURRENCIES_API = "https://curr-convert.onrender.com/initial-currencies";
+export const CONVERT_API = "https://curr-convert.onrender.com/convert";
+export const SORT_CURRENCIES_API = "https://curr-convert.onrender.com/sort-currencies";
