b65fdd89442e32f48d1df8713c2eadb1c0d19a8b
Nick <nikmaunt@gmail.com>
2024-02-13 21:41:51 +0400

    fix: api update

--- src/api/apiConstants.ts
+++ src/api/apiConstants.ts
@@ -1,3 +1,3 @@
-export const INITIAL_CURRENCIES_API = "https://curr-convert.onrender.com/initial-currencies";
-export const CONVERT_API = "https://curr-convert.onrender.com/convert";
-export const SORT_CURRENCIES_API = "https://curr-convert.onrender.com/sort-currencies";
+export const INITIAL_CURRENCIES_API = "https://currency-convert-vlom.onrender.com/initial-currencies";
+export const CONVERT_API = "https://currency-convert-vlom.onrender.com/convert";
+export const SORT_CURRENCIES_API = "https://currency-convert-vlom.onrender.com/sort-currencies";
